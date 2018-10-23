83b92f84067ca9ef5f9488df365db6557de409f2
Akshit Sethi <akshitsethi@gmail.com>
2018-10-23 07:12:46 +0530

    Base files for tests.

---
+++ tests/autoload.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Setup tests.
+ */
+
+define( 'ABSPATH', true );
+
+$root_dir = dirname( dirname(__FILE__) );
+require_once "$root_dir/vendor/autoload.php";
+
+WP_Mock::setUsePatchwork(true);
+WP_Mock::bootstrap();
