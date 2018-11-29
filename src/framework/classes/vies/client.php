3232f4354dcf635a4f0667248c9db6dc03867f4d
Akshit Sethi <akshitsethi@gmail.com>
2018-11-29 13:14:17 +0530

    Add VAT ID validation.

---
+++ src/framework/classes/vies/client.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Niteo\WooCart\BetterTaxHandling\Vies {
+
+  use Exception;
+  use SoapClient;
+  use SoapFault;
+
+  /**
+   * Class that serves as client for processing our request for Tax ID validation.
+   * 
+   * @since 1.0.0
+   */
+  class Client {
+
+    /**
+     * URL to WSDL.
+     *
+     * @var string
+     */
+    private $wsdl = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
+
+    /**
+     * SOAP client.
+     *
+     * @var \SoapClient
+     */
+    private $soapClient;
+
+    /**
+     * SOAP classmap.
+     *
+     * @var array
+     */
+    private $classmap = array(
+      'checkVatResponse' => 'Niteo\WooCart\BetterTaxHandling\Vies\Response'
+    );
+
+    /**
+     * Class constructor.
+     *
+     * @param string|null $wsdl URL to WSDL
+     */
+    public function __construct( $wsdl = null ) {
+      if ( $wsdl ) {
+        $this->wsdl = $wsdl;
+      }
+    }
+
+    /**
+     * Check VAT.
+     *
+     * @param string $countryCode Country code
+     * @param string $vatNumber VAT number
+     *
+     * @return Niteo\WooCart\BetterTaxHandling\Vies\Response
+     * @throws Exception
+     */
+    public function checkVat( $countryCode, $vatNumber ) {
+      try {
+        return $this->getSoapClient()->checkVat(
+          array(
+            'countryCode' => $countryCode,
+            'vatNumber'   => $vatNumber
+          )
+        );
+      } catch( SoapFault $e ) {
+        throw new Exception( 'Error communicating with VIES service.', 0, $e );
+      }
+    }
+
+    /**
+     * Get SOAP client.
+     *
+     * @return \SoapClient
+     */
+    private function getSoapClient() {
+      if ( null === $this->soapClient ) {
+        $this->soapClient = new \SoapClient(
+          $this->wsdl,
+          array(
+            'classmap'    => $this->classmap,
+            'user_agent'  => 'Chrome',
+            'exceptions'  => true
+          )
+        );
+      }
+
+      return $this->soapClient;
+    }
+
+  }
+
+}
---
+++ src/framework/classes/vies/response.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Niteo\WooCart\BetterTaxHandling\Vies {
+
+  /**
+   * Class that serves response to the client.
+   * 
+   * @since 1.0.0
+   */
+  class Response {
+
+    /**
+     * @var string
+     */
+    private $countryCode;
+
+    /**
+     * @var string
+     */
+    private $vatNumber;
+
+    /**
+     * @var string
+     */
+    private $requestDate;
+
+    /**
+     * @var boolean
+     */
+    private $valid;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $address;
+
+    /**
+     * Return country code.
+     *
+     * @return string
+     */
+    public function getCountryCode() {
+      return $this->countryCode;
+    }
+
+    /**
+     * Return VAT number.
+     *
+     * @return string
+     */
+    public function getVatNumber() {
+      return $this->vatNumber;
+    }
+
+    /**
+     * Return Date & Time.
+     *
+     * @return string
+     */
+    public function getRequestDate() {
+      if ( ! $this->requestDate instanceof \DateTime ) {
+        $this->requestDate = new \DateTime( $this->requestDate );
+      }
+
+      return $this->requestDate;
+    }
+
+    /**
+     * Return VAT ID status.
+     *
+     * @return boolean
+     */
+    public function isValid() {
+      return $this->valid;
+    }
+
+    /**
+     * Return name.
+     *
+     * @return string
+     */
+    public function getName() {
+      return $this->name;
+    }
+
+    /**
+     * Return address.
+     *
+     * @return string
+     */
+    public function getAddress() {
+      return $this->address;
+    }
+
+  }
+
+}
