817be9e1318f59a78c7c1e9f73cd930f7f10c755
Akshit Sethi <akshitsethi@gmail.com>
2018-11-30 04:41:53 +0530

    Tests for vies class.

--- tests/autoload.php
+++ tests/autoload.php
@@ -9,10 +9,7 @@ define( 'Version', '1.0' );
 $root_dir = dirname( dirname( __FILE__ ) );
 require_once "$root_dir/vendor/autoload.php";
 
-class WC_Admin_Report {
-	public function check_current_range_nonce() {}
-	public function calculate_current_range() {}
-}
+class WC_Admin_Report {}
 
 WP_Mock::setUsePatchwork(true);
 WP_Mock::bootstrap();
